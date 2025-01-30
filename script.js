dde66bbad2bccff58fbd45f93e44b319d3faaa33
Nim <nimshrestha3@gmail.com>
2025-01-30 20:28:02 +0545

    Replace start & reset button logic with single button logic

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const container = document.querySelector('.gameContainer');
-const startBtn = document.querySelector('#startBtn');
-const resetBtn = document.querySelector('#resetBtn');
+const btn = document.querySelector('#btn');
 const style = getComputedStyle(container);
 
 // slice() to remove 'px' from string returned to prevent NaN in calculations
@@ -10,39 +9,46 @@ const contHeight = style.height.slice(0, style.height.length-2);
 console.log(`Container height: ${contHeight}`);
 
 
-startBtn.addEventListener('click', () => {
-    const sqrsPerSide = prompt('Total squares per side in grid? (Limit: 1 to 100)', 16);
-    console.log(typeof(sqrsPerSide));
-    console.log(`Squares per side: ${sqrsPerSide}`);
-    const totalSqrs = sqrsPerSide*sqrsPerSide;
-    console.log(`Total Squares: ${totalSqrs}`);
-    if (sqrsPerSide > 0 && sqrsPerSide <= 100){
-        for (let i=0;i<totalSqrs;++i){
-            const sqr = document.createElement('div');
-            sqr.classList.add('sqrs');
-            const sqrWidth = contWidth/sqrsPerSide;
-            const sqrHeight = contHeight/sqrsPerSide;
-            sqr.style.width = `${sqrWidth}px`;
-            sqr.style.height = `${sqrHeight}px`;
-            sqr.style.border = 'solid 1px black';
-            container.appendChild(sqr);
-            // check sqr width and height
-            if(i == totalSqrs-1) {
-                console.log(`Square Width: ${sqrWidth}`);
-                console.log(`Square Height: ${sqrHeight}`);
+btn.addEventListener('click', () => {
+    if(btn.textContent === 'Start Sketching'){
+        const sqrsPerSide = prompt('Total squares per side in grid? (Limit: 1 to 100)', 16);
+        console.log(typeof(sqrsPerSide));
+        console.log(`Squares per side: ${sqrsPerSide}`);
+        const totalSqrs = sqrsPerSide*sqrsPerSide;
+        console.log(`Total Squares: ${totalSqrs}`);
+        if (sqrsPerSide > 0 && sqrsPerSide <= 100){
+            for (let i=0;i<totalSqrs;++i){
+                const sqr = document.createElement('div');
+                sqr.classList.add('sqrs');
+                const sqrWidth = contWidth/sqrsPerSide;
+                const sqrHeight = contHeight/sqrsPerSide;
+                sqr.style.width = `${sqrWidth}px`;
+                sqr.style.height = `${sqrHeight}px`;
+                sqr.style.border = 'solid 1px black';
+                container.appendChild(sqr);
+                // check sqr width and height
+                if(i == totalSqrs-1) {
+                    console.log(`Square Width: ${sqrWidth}`);
+                    console.log(`Square Height: ${sqrHeight}`);
+                }
             }
+            btn.textContent = 'Reset';
         }
+        else if (sqrsPerSide == null){}
+        else alert('Invalid input');
         
     }
-    else if (sqrsPerSide == null){}
-    else alert('Invalid input');
-})
-
-resetBtn.addEventListener('click', () => {
-    const sqrs = document.querySelectorAll('.sqrs');
-    sqrs.forEach((sqr)=>{
-        sqr.remove();
-    });
-})
-            
+    else if(btn.textContent === 'Reset'){
+        const decision = confirm('Are you sure you want to reset?');
+        if(decision){
+            console.log(`reset`);
+            const sqrs = document.querySelectorAll('.sqrs');
+            sqrs.forEach((sqr)=>sqr.remove());
+            btn.textContent = 'Start Sketching';
+            let clickEvent = new Event('click');
+            btn.dispatchEvent(clickEvent);
+        }
+        else btn.textContent = 'Reset';
+    }
+}) 
             
