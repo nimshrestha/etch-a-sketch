eb5aac8bb55a4dd2b179b9ec64656c32bb4f3042
Nim <nimshrestha3@gmail.com>
2025-01-30 14:52:03 +0545

    get container width and height

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('.gameContainer');
+const startBtn = document.querySelector('#startBtn')
 const style = getComputedStyle(container);
 const contWidth = style.width;
 const contHeight = style.height;
