8079fec117f23dd9a674bddef41ce69046ebbfd6
Nim <nimshrestha3@gmail.com>
2025-01-30 15:08:46 +0545

    Get squares per side & check validity

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const startBtn = document.querySelector('#startBtn')
 const style = getComputedStyle(container);
 const contWidth = style.width;
 const contHeight = style.height;
+
+startBtn.addEventListener('click', () => {
+    const sqrsPerSide = +prompt('Total squares per side in grid? (Limit: 1 to 100)', 16);
+    console.log(sqrsPerSide);
+    if (sqrsPerSide){
+        
+    }
+})
